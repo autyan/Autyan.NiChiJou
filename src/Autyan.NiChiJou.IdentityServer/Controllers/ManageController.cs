e7a8316140310631b87e141bfde30ef20735727c
Alex Zhang <autyan@hotmail.com>
2018-02-13 22:31:37 +0800

    add manage index view

---
+++ src/Autyan.NiChiJou.IdentityServer/Controllers/ManageController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Autyan.NiChiJou.IdentityServer.Controllers
+{
+    public class ManageController : Controller
+    {
+        // GET
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
