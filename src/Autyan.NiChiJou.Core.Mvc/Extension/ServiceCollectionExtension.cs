39d0718c55410b2aae74619940a7b0d4528b9c25
Alex Zhang <autyan@hotmail.com>
2018-02-20 08:27:53 +0800

    add autyan authentication

---
+++ src/Autyan.NiChiJou.Core.Mvc/Extension/ServiceCollectionExtension.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Autyan.NiChiJou.Core.Config;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Autyan.NiChiJou.Core.Mvc.Extension
+{
+    public static class ServiceCollectionExtension
+    {
+        public static IServiceCollection AddAutyanAuthentication(this IServiceCollection services)
+        {
+            services.AddAuthentication(ResourceConfiguration.AuthenticationScheme)
+                .AddCookie(ResourceConfiguration.AuthenticationScheme, options =>
+                {
+                    options.LoginPath = ResourceConfiguration.LoginPath;
+                    options.LogoutPath = ResourceConfiguration.LogoutPath;
+                    options.ExpireTimeSpan = TimeSpan.FromMinutes(ResourceConfiguration.CookieExpiration);
+                });
+        }
+    }
+}
