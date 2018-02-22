b4f9be80491fd65c502a800ae7b10e005988233e
Alex Zhang <autyan@hotmail.com>
2018-02-22 23:57:12 +0800

    fill tokenbasedserviceauthentication

--- src/Autyan.NiChiJou.Core.Mvc/Authorization/TokenBasedServiceAuthenticationOptions.cs
+++ src/Autyan.NiChiJou.Core.Mvc/Authorization/TokenBasedServiceAuthenticationOptions.cs
@@ -4,6 +4,8 @@ namespace Autyan.NiChiJou.Core.Mvc.Authorization
 {
     public class TokenBasedServiceAuthenticationOptions : AuthenticationSchemeOptions
     {
+        public ulong RequestMaxAgeSeconds { get; set; }
 
+        public string AuthenticationSchema { get; set; }
     }
 }
