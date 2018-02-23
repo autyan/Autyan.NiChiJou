dd0f416f9e14e535f6dfe9e7c57590a4d5fb0a9e
Alex Zhang <autyan@hotmail.com>
2018-02-23 22:05:58 +0800

    remove noncen code.

--- src/Autyan.NiChiJou.Core.Mvc/AuthorizationPolicyDefinition.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Autyan.NiChiJou.Core.Mvc
-{
-    public static class AuthorizationPolicyDefinition
-    {
-        public const string CookiePolicy = nameof(CookiePolicy);
-
-        public const string ServerTokenPolicy = nameof(ServerTokenPolicy);
-    }
-}
